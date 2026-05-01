ea4a7cea7ec6fb9b634e699898e3121eab41778f
Кострубин Артём <kam182002@gmail.com>
2026-05-01 13:39:06 +0300

    Update timer.js
    edit data

--- js/timer.js
+++ js/timer.js
@@ -19,7 +19,7 @@ const timer = () => {
 
     const updateTimer = () => {
         const date = new Date();
-        const dateDeadline = new Date('12 november 2025').getTime();
+        const dateDeadline = new Date('12 august 2026').getTime();
         const timeRemaining = (dateDeadline - date) / 1000;
 
 
